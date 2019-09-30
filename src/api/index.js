e168ae8bb5aa413b14412a45982053b08a1bbecc
Marian Hello <m.hello@gmail.com>
2019-09-30 17:52:02 +0200

    add https

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ const API_KEY = 'b947c005';
 
 export async function fetchMovie(id) {
     const response = await fetch(
-      `http://omdbapi.com?apikey=${API_KEY}&i=${id}`
+      `https://omdbapi.com?apikey=${API_KEY}&i=${id}`
     );
     if (response.status >= 200 && response.status < 300) {
       return await response.json();
@@ -18,7 +18,7 @@ export async function searchMovies(text) {
   // Note: In poduction add fetch polyfill
 
     const response = await fetch(
-      `http://omdbapi.com?apikey=${API_KEY}&s=${text}`
+      `https://omdbapi.com?apikey=${API_KEY}&s=${text}`
     );
     if (response.status >= 200 && response.status < 300) {
       return await response.json();
